ee9cd14f5431328e9a5f2501dbe5544fa875ac76
tatsumara <sutakumu@gmail.com>
2020-12-09 22:32:37 +0100

    Added usage

--- commands/img.js
+++ commands/img.js
@@ -4,6 +4,7 @@ module.exports = {
 	name: 'img',
 	aliases: ['image', 'search'],
 	description: 'Searches for an image on DuckDuckGo.',
+	usage: 'img [search query]',
 	execute(client, message, args) {
 		ddgi.image_search({ query: args.join(' ') })
 			.then(results=>{
