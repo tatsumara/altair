2b4e6be752d099326effcaf9f10f8926d3b90d2b
tatsumara <sutakumu@gmail.com>
2020-12-09 11:12:19 +0100

    Modified to show system information

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,19 @@
+const os = require('os');
+const Discord = require('discord.js');
+
 module.exports = {
 	name: 'ping',
-	description: 'Ping!',
+	description: 'Ping! Sends a bit of running information.',
 	execute(client, message) {
 		message.channel.send('Pong.');
+		const content = new Discord.MessageEmbed()
+			.setColor('#0073E6')
+			.setTitle('Altair Environment')
+			.setDescription(`Hostname: ${os.hostname()}
+			Platform: ${os.platform()}
+			Release: ${os.release()}
+			Uptime: ${os.uptime()}s
+			`);
+		message.channel.send(content);
 	},
 };
