41e7a9db69ccdcf40781f51d1849b72e7b6cc8b9
tatsumara <sutakumu@gmail.com>
2021-03-09 10:03:08 +0100

    Added aliases and removed image command

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ client.on('message', message => {
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const commandName = args.shift().toLowerCase();
 
-	if (!client.commands.has(commandName)) return;
-	const command = client.commands.get(commandName);
+	const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+	if (!command) return;
 
 	if (command.disabled) {
 		const disabledEmbed = {
