84ca9c31189d4357fc705c04341aeeb2fee6c071
tatsumara <sutakumu@gmail.com>
2021-03-10 19:14:53 +0100

    Fixed rockpost

--- commands/rockpost.js
+++ commands/rockpost.js
@@ -4,7 +4,7 @@ module.exports = {
     aliases: ['aliases'],
 	execute(client, message, args, functions) {
         if (args[0] > 6) {
-            return message.channel.send(functions.ezEmbed('Maximum of 6 rockposts allowed at once.', '', '0xFF0000'));
+            return message.channel.send(functions.simpleEmbed('Maximum of 6 rockposts allowed at once.', '', '0xFF0000'));
         }
         const rocks = ['https://media.discordapp.net/attachments/708816464911532032/819269670048759860/rocks_-_Copy_-_Copy_2.gif', 'https://media.discordapp.net/attachments/708816464911532032/819269677217480734/rocks_2_-_Copy_-_Copy.gif', 'https://media.discordapp.net/attachments/708816464911532032/819269731432529990/rocks_10_-_Copy_-_Copy.gif'];
         for (i = 0; i<args[0]; i++) {
