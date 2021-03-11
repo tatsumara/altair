2f54b37dd0d31621ff69e70e4604e5bcba6490df
tatsumara <sutakumu@gmail.com>
2021-03-11 23:06:19 +0100

    Added page functionality to image command

--- commands/image.js
+++ commands/image.js
@@ -13,11 +13,22 @@ module.exports = {
             queryStringAddition: '&safe=active'
         }
         gis(options, logResults); 
-        function logResults(error, results) {
+        async function logResults(error, results) {
             if (!results[0]) {
                 return message.channel.send(functions.simpleEmbed('Nothing found!', ''));
             }
-            message.channel.send(functions.simpleEmbed(`Search results for '${query}':`, '', '0x0000FF', results[0].url));
+            let x = 0;
+            const imageEmbed = await message.channel.send(functions.simpleEmbed(`Image Search Results:`, `"${query}": Page ${x}`, '0x0000FF', results[x].url));
+            const expiration = Date.now() + 60000;
+            const filter = m => m.author.id === message.author.id;
+            while (Date.now() < expiration) {
+                const collected = await imageEmbed.channel.awaitMessages(filter, { max: 1, time: 60000 })
+                if (collected.array()[0].content.toLowerCase() === 'n') x++
+                else if (collected.array()[0].content.toLowerCase() === 'b' && x > 0) x--
+                else continue;
+                collected.array()[0].delete();
+                imageEmbed.edit(functions.simpleEmbed(`Image Search Results:`, `"${query}": Page ${x}`, '0x0000FF', results[x].url))
+            }
         }
         message.channel.stopTyping();
 	},
