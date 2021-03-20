2ce526060074dc1cdb4543fd0bd605a22670c52d
tatsumara <sutakumu@gmail.com>
2021-03-20 23:47:51 +0100

    Added multi word support to urban

--- commands/urban.js
+++ commands/urban.js
@@ -9,13 +9,14 @@ module.exports = {
 		if (!args[0]) {
             return message.channel.send(functions.simpleEmbed('Please run this command with the word you want to look up.', '', '0xFFFF00'))
         }
-        if (args.length > 1) {
-            return message.channel.send(functions.simpleEmbed('Please only run this command with one word.', '', '0xFFFF00'))
-        }
-		got(`http://api.urbandictionary.com/v0/define?term=${args[0]}`).then(res =>{
+        const term = encodeURIComponent(args.join(' '))
+		got(`http://api.urbandictionary.com/v0/define?term=${term}`).then(res =>{
             const result = JSON.parse(res.body);
+            if (!result.list[0]) {
+                return message.channel.send(functions.simpleEmbed('Nothing found!', ''))
+            }
             const embed = new Discord.MessageEmbed()
-				.setTitle(`UrbanDictionary: "${args[0]}"`)
+				.setTitle(`UrbanDictionary: "${args.join(' ')}"`)
 				.setColor('0x0000FF')
 				.setDescription(result.list[0].definition)
             message.channel.send(embed)
