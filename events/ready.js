c17971726545d44f126dcc1e8aa8d25b268be47d
tatsumara <sutakumu@gmail.com>
2021-03-24 21:31:26 +0100

    Minor cosmetic changes

--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,7 @@ module.exports = {
     name: 'ready',
     once: 'true',
     execute(client) {
+        client.user.setActivity('the quadrant.', { type: 'WATCHING'});
         console.log(chalk.blueBright(`[altr] Ready to serve on ${client.guilds.cache.size} servers!`));
     }
 }
