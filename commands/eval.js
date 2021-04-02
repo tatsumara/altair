447e7b153456fddc27d371f7659481125f0f60af
tatsumara <sutakumu@gmail.com>
2021-04-02 11:55:04 +0200

    Taking advantage of text file embeds

--- commands/eval.js
+++ commands/eval.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'eval',
 	description: 'Enables the owner to execute commands directly from inside discord.',
     owner: true,
-	execute(client, message, args, functions) {
+	async execute(client, message, args, functions) {
         if (message.author.id !== require('../config.json').ownerID) {
             return message.channel.send(functions.simpleEmbed('You are not permitted to use this command.', '','#FF0000'));
         };
@@ -10,10 +10,13 @@ module.exports = {
             let evaled = eval(args.join(' '));
             if (typeof evaled !== 'string') {
                 // actually didn't know you could use require like this but it makes sense and is pretty cool if you only use something once
-                evaled = require('util').inspect(evaled);
+                evaled = require('util').inspect(await evaled);
             }
             // i don't understand how the code is actually executed
-            message.channel.send(evaled, {code:"js"});
+            message.channel.send({ files: [{
+                attachment: Buffer.from(evaled),
+                name: 'evaled.rb'
+            }]});
         } catch (err) {
             message.channel.send(err, {code: "js"});
         }
