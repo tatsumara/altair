f25c72b0cd82767cdf6fb98d3520d99f873365cd
tatsumara <sutakumu@gmail.com>
2021-04-21 17:03:36 +0200

    Implemented userinfo command

---
+++ commands/userinfo.js
@@ -0,0 +1,25 @@
+module.exports = {
+	name: 'userinfo',
+	description: 'command description',
+	cooldown: '5',
+	args: true,
+    aliases: ['ui', 'uinfo', 'user'],
+	async execute(client, message, args, functions) {
+		const user = message.mentions.members.first().user || await client.users.fetch(args[0])
+		const member = message.guild.member(user)
+        const embed = {
+			color: '#0073E6',
+			title: `${user.tag}`,
+			thumbnail: {
+				url: user.avatarURL()
+			},
+			fields: [
+				{ name: 'ID', value: user.id, inline: true },
+				{ name: 'Created at', value: user.createdAt.toDateString(), inline: true },
+				{ name: 'Joined at', value: member.joinedAt.toDateString(), inline: true },
+				{ name: 'Roles', value: member.roles.cache.array().toString(), inline: true },
+			]
+		}
+		message.channel.send({ embed: embed })
+	},
+};
