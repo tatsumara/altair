faf909cb9211bf70c1981ed31b4b7daec31778e6
tatsumara <sutakumu@gmail.com>
2021-05-06 15:56:14 +0200

    Added category to commands

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ for (const folder of commandFolders) {
 	const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
 	for (const file of commandFiles) {
 		const command = require(`./commands/${folder}/${file}`);
+		command.category = folder;
 		client.commands.set(command.name, command);
 	}
 }
