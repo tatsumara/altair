42b5dbf0ca3f35ad5d99a2a9b0c46733b75eaae6
tatsumara <sutakumu@gmail.com>
2021-05-17 12:28:45 +0200

    Added serverinfo command

---
+++ commands/utility/serverinfo.js
@@ -0,0 +1,26 @@
+module.exports = {
+	name: 'server',
+	description: 'Shows info about the current server.',
+	cooldown: '5',
+	guildOnly: true,
+	aliases: ['si', 'sinfo', 'server'],
+	execute(client, message) {
+		const embed = {
+			color: '#0073E6',
+			title: `${message.guild.name}`,
+			thumbnail: {
+				url: message.guild.iconURL({ size: 4096, dynamic: true }),
+			},
+			fields: [
+				{ name: 'ID', value: message.guild.id, inline: true },
+				{ name: 'Created at', value: message.guild.createdAt.toDateString(), inline: true },
+				{ name: 'Owner', value: message.guild.owner, inline: true },
+				{ name: 'Boosts', value: message.guild.premiumSubscriptionCount, inline: true },
+				{ name: 'Members', value: message.guild.memberCount, inline: true },
+				{ name: 'Features', value: message.guild.features.join(', ') },
+				{ name: 'Roles', value: message.guild.roles.cache.array().join(' '), inline: true },
+			],
+		};
+		message.channel.send({ embed: embed });
+	},
+};
