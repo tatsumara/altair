36ad81f1f78caf2b63207e812dbf5a5cd095c28a
tatsumara <sutakumu@gmail.com>
2021-06-26 21:18:14 +0200

    Changed avatar command to work like userinfo

--- commands/fun/avatar.js
+++ commands/fun/avatar.js
@@ -3,14 +3,17 @@ module.exports = {
 	description: 'Sends your or the mentioned users avatar.',
 	usage: 'avatar [user]',
 	cooldown: '5',
+	guildOnly: true,
 	aliases: ['av', 'pfp'],
-	execute(client, message) {
-		let user = '';
-		if (message.mentions.members?.first() && message.channel.type !== 'dm') {
-			user = message.mentions.members.first().user;
-		} else {
-			user = message.author;
+	async execute(client, message, args, functions) {
+		const userID = args[0]?.replace(/<@!|>/g, '') || message.author.id;
+		if (!userID.match('\\d{17,18}')) return message.channel.send(functions.simpleEmbed('Not a valid mention or ID!', '', '#FFFF00'));
+		let member = '';
+		try {
+			member = await message.guild.members.fetch(userID);
+		} catch {
+			return message.channel.send(functions.simpleEmbed('No user found!', 'This command can only show the avatar of users in the current server.'));
 		}
-		message.channel.send(user.avatarURL({ size: 2048, dynamic: true }));
+		message.channel.send(member.user.avatarURL({ size: 2048, dynamic: true }));
 	},
 };
