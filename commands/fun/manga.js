85d659fc22102e721f21a8d64a8241918d905ef6
portasynthinca3 <portasynthinca3@gmail.com>
2021-08-10 23:18:16 +0300

    ig that should fix it

--- commands/fun/manga.js
+++ commands/fun/manga.js
@@ -12,12 +12,15 @@ module.exports = {
 		const data = await anilist.searchEntry.manga(args.join(' '));
 		if (data.pageInfo.total === 0) return message.channel.send(functions.simpleEmbed('Nothing found!', ''));
 		const manga = await anilist.media.manga(data.media[0].id);
+		const alternateSpellings = manga.title.english === null
+				? manga.title.native
+				: `${manga.title.english}, ${manga.title.native}`;
 		const embed = {
 			color: manga.coverImage.color,
 			title: `[${manga.format}] ${manga.title.romaji}`,
 			url: manga.siteUrl,
 			fields: [
-				{ name: 'Alternate Spellings:', value: `${manga.title.english}, ${manga.title.native}`, inline: true },
+				{ name: 'Alternate Spellings:', value: alternateSpellings, inline: true },
 				{ name: 'Start date:', value: `${manga.startDate.day || '--'}.${manga.startDate.month || '--'}.${manga.startDate.year || '----'}`, inline: true },
 				{ name: 'End date:', value: `${manga.endDate.day || '--' }.${manga.endDate.month || '--' }.${manga.endDate.year || '----' }`, inline: true },
 				{ name: 'Status:', value: `${manga.status} with ${manga.volumes || 'unknown'} volumes`, inline: true },
