5d03a33a623cfd13b7332742b7291e3f71e48cfe
tatsumara <sutakumu@gmail.com>
2021-09-12 19:50:24 +0200

    Added check for wolfram api key

--- commands/fun/wolframalpha.js
+++ commands/fun/wolframalpha.js
@@ -1,5 +1,6 @@
 const { MessageEmbed } = require('discord.js');
 const got = require('got');
+const chalk = require('chalk');
 
 module.exports = {
 	name: 'wolframalpha',
@@ -8,6 +9,7 @@ module.exports = {
 	args: true,
 	aliases: ['wolfram', 'walpha'],
 	execute(client, message, args, functions) {
+		if (!process.env.WOLFRAM_API_KEY) return console.log(chalk.red('[cmnd] Please input your WolframAlpha API key in the config.'));
 		got(`http://api.wolframalpha.com/v2/query?appid=${process.env.WOLFRAM_API_KEY}&output=json&input=${encodeURIComponent(args.join(' '))}`, { responseType: 'json' }).then(res =>{
 			const pods = res.body.queryresult.pods;
 			if (!pods) return message.channel.send(functions.simpleEmbed('No result!', ''));
