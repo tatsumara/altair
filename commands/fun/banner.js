8184ef6f664b57974f9f6fbb9095e1c141531244
tatsumara <sutakumu@gmail.com>
2021-10-05 20:43:27 +0200

    Added banner command

--- commands/fun/banner.js
+++ commands/fun/banner.js
@@ -1,23 +1,21 @@
-const { DiscordBanners } = require('discord-banners');
-
 module.exports = {
 	name: 'banner',
 	description: 'Sends your or the mentioned users banner.',
 	usage: 'avatar [user]',
 	cooldown: '5',
-	disabled: true,
 	guildOnly: true,
 	async execute(client, message, args, functions) {
-		const b = new DiscordBanners(client);
 		let member = message.mentions.users.first() || args[0]?.match(/\d{17,18}/) || message.author;
 		if (Array.isArray(member)) {
-			member = await client.users.cache.get(member[0]);
+			member = await client.users.fetch(member[0]);
 		}
 		if (!member) return message.channel.send(functions.simpleEmbed('User not found or not a user!'));
-		if (member.bot) return message.channel.send(functions.simpleEmbed('This command does not work for bots yet!'));
-		let banner = await b.getBanner(member.id, { size: 4096, dynamic: true });
-		if (banner.startsWith('#')) banner = functions.simpleEmbed(`Color: ${banner}`, '', banner);
-		if (!banner) return message.channel.send(functions.simpleEmbed('No banner available!'));
+		await member.fetch();
+
+		const banner = await member.bannerURL({ size: 4096, dynamic: true });
+
+		if (!banner) return message.channel.send(functions.simpleEmbed('User does not have a banner.'));
+
 		message.channel.send(banner);
 	},
 };
