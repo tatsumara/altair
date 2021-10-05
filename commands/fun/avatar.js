0425c93116ade3507614bed06873ef9b597e5811
tatsumara <sutakumu@gmail.com>
2021-10-05 20:43:53 +0200

    Aligned avatar command with banner command

--- commands/fun/avatar.js
+++ commands/fun/avatar.js
@@ -8,7 +8,7 @@ module.exports = {
 	async execute(client, message, args, functions) {
 		let member = message.mentions.users.first() || args[0]?.match(/\d{17,18}/) || message.author;
 		if (Array.isArray(member)) {
-			member = await client.users.cache.get(member[0]);
+			member = await client.users.fetch(member[0]);
 		}
 		if (!member) return message.channel.send(functions.simpleEmbed('User not found or not a user!'));
 		message.channel.send(member.displayAvatarURL({ size: 4096, dynamic: true }));
