1bb0df8bce8519c151dc102b0076a1f47a07b0e5
tatsumara <sutakumu@gmail.com>
2021-10-09 16:30:30 +0200

    Avatar now displays the guild avatar

--- commands/fun/avatar.js
+++ commands/fun/avatar.js
@@ -6,9 +6,9 @@ module.exports = {
 	guildOnly: true,
 	aliases: ['av', 'pfp'],
 	async execute(client, message, args, functions) {
-		let member = message.mentions.users.first() || args[0]?.match(/\d{17,18}/) || message.author;
+		let member = message.mentions.members.first() || args[0]?.match(/\d{17,18}/) || message.member;
 		if (Array.isArray(member)) {
-			member = await client.users.fetch(member[0]);
+			member = await message.guild.members.fetch(member[0]);
 		}
 		if (!member) return message.channel.send(functions.simpleEmbed('User not found or not a user!'));
 		message.channel.send(member.displayAvatarURL({ size: 4096, dynamic: true }));
