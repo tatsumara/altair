a70009aabcf55a1a0e78f4b9f2ca3fbbb242aaeb
portasynthinca3 <portasynthinca3@gmail.com>
2022-01-25 17:27:49 +0300

    feat: translation target language selection

--- commands/knowledge/translate.js
+++ commands/knowledge/translate.js
@@ -5,15 +5,24 @@ const chalk = require('chalk');
 module.exports = {
 	name: 'translate',
 	description: 'Translates a string of text into English.',
-	usage: 'translate <text>',
+	usage: 'translate [!<target language>] <text>',
 	cooldown: '10',
 	args: true,
 	aliases: ['tr'],
 	execute(client, message, args, functions) {
+		// check DEEPL_API_KEY
 		if (!process.env.DEEPL_API_KEY) {
 			return console.log(chalk.red('[cmnd] Please input your DeepL API key in the config.'));
 		}
-		// perform length checks
+
+		// choose target language
+		let target = 'EN';
+		if (args[0].startsWith('!')) {
+			target = args[0].slice(1).toUpperCase();
+			[_, ...args] = args; // remove first element
+		}
+
+		// check length
 		const text = args.join(' ');
 		if (text.length >= 120) {
 			return message.channel.send(functions.simpleEmbed('Text length exceeds 120 characters.'));
@@ -22,7 +31,7 @@ module.exports = {
 		// query the API
 		translate({
 			text,
-			target_lang: 'EN',
+			target_lang: target,
 			auth_key: process.env.DEEPL_API_KEY,
 			free_api: true,
 		}).then(({ data }) => {
