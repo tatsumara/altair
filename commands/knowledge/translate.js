adbb09179a1406b9fc82ec2b608ce14b3098bbc1
portasynthinca3 <portasynthinca3@gmail.com>
2022-01-25 17:21:03 +0300

    style: separate DEEPL_API_KEY message from condition

--- commands/knowledge/translate.js
+++ commands/knowledge/translate.js
@@ -10,7 +10,9 @@ module.exports = {
 	args: true,
 	aliases: ['tr'],
 	execute(client, message, args, functions) {
-		if (!process.env.DEEPL_API_KEY) return console.log(chalk.red('[cmnd] Please input your DeepL API key in the config.'));
+		if (!process.env.DEEPL_API_KEY) {
+			return console.log(chalk.red('[cmnd] Please input your DeepL API key in the config.'));
+		}
 		// perform length checks
 		const text = args.join(' ');
 		if (text.length >= 120) {
