0c6a495fe1f4b6b6ee0d052190fc5d66c729eadb
portasynthinca3 <portasynthinca3@gmail.com>
2022-01-25 17:01:26 +0300

    feat: remove daily limit

--- commands/knowledge/translate.js
+++ commands/knowledge/translate.js
@@ -2,29 +2,6 @@ const { MessageEmbed } = require('discord.js');
 const translate = require('deepl');
 const chalk = require('chalk');
 
-// make sure we don't make salad pay money
-
-const dailyLimit = Math.floor(500_000 / 31);
-let usedToday = 0;
-
-function invokeAtMidnight(fun) {
-	const now = new Date();
-	let millisLeft = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0) - now;
-	// it's past midnight already, set timer to midnight of next day
-	if (millisLeft <= 0) {
-		millisLeft += 24 * 3600 * 1000;
-	}
-	setTimeout(fun, millisLeft);
-	console.log(chalk.blue(`[trlt] running hook in ${millisLeft}ms`));
-}
-
-function resetUsage() {
-	usedToday = 0;
-	console.log(chalk.blue('[trlt] usage reset'));
-	invokeAtMidnight(resetUsage);
-}
-invokeAtMidnight(resetUsage);
-
 module.exports = {
 	name: 'translate',
 	description: 'Translates a string of text into English.',
@@ -37,9 +14,6 @@ module.exports = {
 		if (text.length >= 120) {
 			return message.channel.send(functions.simpleEmbed('Text length exceeds 120 characters'));
 		}
-		if (text.length + usedToday >= dailyLimit) {
-			return message.channel.send(functions.simpleEmbed('No characters left for today'));
-		}
 
 		// query the API
 		translate({
@@ -49,13 +23,11 @@ module.exports = {
 			free_api: true,
 		}).then(({ data }) => {
 			const { detected_source_language: lang, text: translated } = data.translations[0];
-			usedToday += text.length;
 			// send response
 			const embed = new MessageEmbed()
 				.setTitle(`Translated from '${lang}'`)
 				.setColor('#0073E6')
-				.setDescription(translated)
-				.setFooter({ text: `${dailyLimit - usedToday}/${dailyLimit} characters left today` });
+				.setDescription(translated);
 			return message.channel.reply({ embeds: [embed] });
 		}).catch(err => {
 			// send error message
