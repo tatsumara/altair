93b728e8cf670612eac05c46fb561f4027ac9f90
portasynthinca3 <portasynthinca3@gmail.com>
2022-01-25 19:57:51 +0300

    feat: freeform language names in translate

--- commands/knowledge/translate.js
+++ commands/knowledge/translate.js
@@ -1,5 +1,7 @@
 const { MessageEmbed } = require('discord.js');
 const translate = require('deepl');
+const got = require('got');
+const sift3 = require('sift3');
 const chalk = require('chalk');
 
 function langToFlag(lang) {
@@ -9,10 +11,47 @@ function langToFlag(lang) {
 	return `:flag_${lang}:`;
 }
 
+let langs = [];
+function queryLangs() {
+	return new Promise((resolve, reject) => {
+		if (!process.env.DEEPL_API_KEY) reject('no DEEPL_API_KEY env var');
+		const form = {
+			auth_key: process.env.DEEPL_API_KEY,
+			type: 'target',
+		};
+		got.post('https://api-free.deepl.com/v2/languages', { form }).then(({ body }) => {
+			console.log(chalk.blue('[trlt] Loaded available languages'));
+			resolve(JSON.parse(body));
+		});
+	});
+}
+queryLangs().then(l => langs = l);
+
+function findLang(target) {
+	let best = '';
+	let bestDist = 100;
+	for(const { language, name } of langs) {
+		if (name.toLowerCase().includes(target))
+			return language;
+		const dist = sift3(target, name);
+		if (dist <= bestDist) {
+			best = language;
+			bestDist = dist;
+		}
+	}
+	if (bestDist >= 4)
+		return '';
+	return best;
+}
+
 module.exports = {
 	name: 'translate',
 	description: 'Translates a string of text into English.',
-	usage: 'translate [!<target language>] <text>',
+	usage: 'translate [{<target language>} OR !<target language>] <text>',
+	examples: [
+		'translate !de i like cookies',
+		'translate {German} i like cookies',
+	],
 	cooldown: '10',
 	args: true,
 	aliases: ['tr'],
@@ -28,6 +67,14 @@ module.exports = {
 			target = args[0].slice(1).toUpperCase();
 			[_, ...args] = args; // remove first element
 		}
+		if (args[0].startsWith('{') && args[0].endsWith('}')) {
+			const name = args[0].substr(1, args[0].length - 2);
+			target = findLang(name);
+			if (target === '') {
+				return message.channel.send(functions.simpleEmbed('I don\'t know this language.'));
+			}
+			[_, ...args] = args;
+		}
 
 		// check length
 		const text = args.join(' ');
@@ -42,10 +89,10 @@ module.exports = {
 			auth_key: process.env.DEEPL_API_KEY,
 			free_api: true,
 		}).then(({ data }) => {
-			const { detected_source_language: source, text: translated } = data.translations[0];
+			const { text: translated } = data.translations[0];
 			// send response
 			const embed = new MessageEmbed()
-				.setTitle(`Translated ${langToFlag(source)} into ${langToFlag(target)}:`)
+				.setTitle(`Translated into ${langToFlag(target)}:`)
 				.setColor('#0073E6')
 				.setDescription(translated);
 			return message.reply({ embeds: [embed] });
