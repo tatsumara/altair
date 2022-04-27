7c34ed66fa9c16920d0a7e55b0743d4a13594264
Anna Antonenko <hi@portasynthinca3.me>
2022-04-27 23:29:58 +0300

    fix: discord.js supports streamed input :rolling_eyes:

--- commands/images/cat.js
+++ commands/images/cat.js
@@ -6,18 +6,7 @@ module.exports = {
 	description: 'Sends an image of a cat.',
 	guildOnly: false,
 	async execute(client, message) {
-		// download file locally
-		// we can't just send a link to the website because
-		// of how discord caching works
-		const path = `cat_${message.id}.jpg`;
 		const stream = got.stream('https://thiscatdoesnotexist.com/');
-		stream.pipe(fs.createWriteStream(path));
-
-		stream.on('end', async () => {
-			// send file
-			await message.reply({ files: [path] });
-			// remove file
-			fs.unlinkSync(path);
-		});
+		await message.reply({ files: [stream] });
 	},
 };
