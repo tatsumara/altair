7b7c95d06be8c758c91bbd51f5d7f5ee8d5bb594
tatsumara <mara@tatsumara.dev>
2022-05-06 19:20:33 +0200

    fix: add missing api key error

--- commands/images/apod.js
+++ commands/images/apod.js
@@ -6,6 +6,7 @@ module.exports = {
 	description: 'Displays NASA\'s Astronomy Picture of the Day.',
 	aliases: ['nasa'],
 	async execute(client, message) {
+		if (!process.env.NASA_API_KEY) return client.log.error('Please input your NASA API key in the config.');
 		got(`https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}`).then(res => {
 			res = JSON.parse(res.body);
 			const embed = new MessageEmbed()
