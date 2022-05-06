bdeeb03d6ecd75a410c9c3e191481160d71cc774
tatsumara <mara@tatsumara.dev>
2022-05-06 19:19:48 +0200

    fix: fix missing api key error

--- commands/knowledge/wolframalpha.js
+++ commands/knowledge/wolframalpha.js
@@ -8,7 +8,7 @@ module.exports = {
 	disabled: false,
 	aliases: ['wolfram', 'walpha', 'wa'],
 	async execute(client, message, args) {
-		if (!process.env.WOLFRAM_API_KEY) return client.log.info('Please input your WolframAlpha API key in the config.');
+		if (!process.env.WOLFRAM_API_KEY) return client.log.error('Please input your WolframAlpha API key in the config.');
 		try {
 			const res = await got(`http://api.wolframalpha.com/v1/spoken?appid=${process.env.WOLFRAM_API_KEY}&i=${encodeURIComponent(args.join(' '))}`);
 			message.reply(res.body + '.');
