a21b7097ad839ef488809ccaaf27d9e01ee09a41
tatsumara <mara@tatsumara.dev>
2022-05-24 16:57:10 +0200

    feat: add error handling for catsay

--- commands/fun/catsay.js
+++ commands/fun/catsay.js
@@ -5,11 +5,14 @@ module.exports = {
 	description: 'Let a cat say something.',
 	usage: 'catsay <message>',
 	args: true,
-	async execute(client, message, args) {
+	async execute(client, message, args, functions) {
 		const msg = args.join(' ');
 		const url = `https://cataas.com/cat/says/${encodeURIComponent(msg)}`;
-		const stream = got.stream(url);
-
-		await message.reply({ files: [stream] });
+		try {
+			const stream = got.stream(url);
+			await message.reply({ files: [stream] });
+		} catch {
+			message.reply(functions.simpleEmbed('`Cats as a service` is currently unavailable. Please try again later.'));
+		}
 	},
 };
