eda0a3a56745360555da6a2e828ff46ff67e45d1
tatsumara <mara@tatsumara.dev>
2022-08-04 03:41:10 +0200

    feat: add banner and icon to serverinfo

--- commands/knowledge/serverinfo.js
+++ commands/knowledge/serverinfo.js
@@ -1,24 +1,55 @@
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+
 module.exports = {
 	name: 'serverinfo',
 	description: 'Shows info about the current server.',
 	guildOnly: true,
 	aliases: ['si', 'sinfo', 'server'],
 	async execute(client, message) {
-		const embed = {
-			color: client.colors.blue,
-			title: `${message.guild.name}`,
-			thumbnail: {
-				url: message.guild.iconURL({ size: 4096, dynamic: true }),
-			},
-			fields: [
+		const mainEmbed = new MessageEmbed()
+			.setColor(client.colors.blue)
+			.setTitle(message.guild.name)
+			.setThumbnail(message.guild.iconURL({ size: 4096, dynamic: true }))
+			.addFields([
 				{ name: 'ID', value: message.guild.id.toString(), inline: true },
 				{ name: 'Created at', value: message.guild.createdAt.toDateString(), inline: true },
 				{ name: 'Owner', value: `${await message.guild.fetchOwner()}`, inline: true },
 				{ name: 'Boosts', value: message.guild.premiumSubscriptionCount.toString(), inline: true },
 				{ name: 'Members', value: message.guild.memberCount.toString(), inline: true },
-				{ name: 'Features', value: message.guild.features.join(', ') || 'NONE', inline: true },
-			],
+				{ name: 'Features', value: message.guild.features?.join(', ') || 'NONE' },
+			]);
+
+		const buttons = new MessageActionRow()
+			.addComponents(
+				new MessageButton({ label: 'Server Icon', customId: 'icon', style: 'SECONDARY' }),
+			);
+		if (message.guild.banner) buttons.addComponents(new MessageButton({ label: 'Banner', customId: 'banner', style: 'SECONDARY' }));
+
+		const msg = await message.reply({ embeds: [mainEmbed], components: [buttons] });
+		const filter = i => {
+			i.deferUpdate();
+			return i.user.id === message.author.id;
 		};
-		return message.channel.send({ embeds: [embed] });
+		const collector = msg.createMessageComponentCollector({ filter, idle: 30000 });
+
+		collector.on('collect', i => {
+			if (i.customId === 'icon') {
+				const iconURL = message.guild.iconURL({ size: 4096, dynamic: true });
+				const iconEmbed = new MessageEmbed()
+					.setColor(client.colors.blue)
+					.setTitle('Open Original')
+					.setURL(iconURL)
+					.setImage(iconURL);
+				msg.edit({ embeds: [iconEmbed] });
+			} else {
+				const bannerURL = message.guild.bannerURL({ size: 4096, dynamic: true });
+				const bannerEmbed = new MessageEmbed()
+					.setColor(client.colors.blue)
+					.setTitle('Open Original')
+					.setURL(bannerURL)
+					.setImage(bannerURL);
+				msg.edit({ embeds: [bannerEmbed] });
+			}
+		});
 	},
 };
