8f35f12f8191e0f799ab9f641a4b6eeeab32752f
salad <104382405+tatsusalad@users.noreply.github.com>
2022-10-17 23:57:57 +0300

    Added reddit command
    pulls images from your subreddit of choice! pretty shite but works

---
+++ commands/images/reddit.js
@@ -0,0 +1,80 @@
+const { MessageEmbed, MessageActionRow, MessageButton, Guild, GuildChannel, Channel, Message } = require('discord.js');
+const snoowrap = require('snoowrap');
+const r = new snoowrap({
+	userAgent: 'Mozilla/5.0',
+	clientId: process.env.CLIENT_ID,
+	clientSecret: process.env.CLIENT_SECRET,
+	refreshToken: process.env.REFRESH_TOKEN,
+});
+module.exports = {
+	name: 'reddit',
+	description: 'Shows images from a subreddit.',
+	usage: 'reddit <search query>',
+	cooldown: '10',
+	args: true,
+	aliases: ['red', 'reddit'],
+	async execute(client, message, args, functions) {
+		let subName = args.at(0);
+		console.log(args, args.length);
+		if (args.length > 1) {
+			return message.reply(functions.simpleEmbed('Please provide a single subreddit!'));
+		}
+		let subreddit = await r.getHot(subName)
+		
+// stealing mara's code, hopefully works
+// making x = 1 since most subreddits have a shitty pinned post. could i make it not show the pinned post? probably. do i care enough to do that? no
+let x = 1;
+		let imageLink = subreddit.at(x).url
+		let postTitle = subreddit.at(x).title
+		let nsfw = subreddit.at(x).over_18
+		if (nsfw === true & message.channel.nsfw === false){
+			return message.reply(functions.simpleEmbed('Cannot post nsfw content in sfw channel!'))
+		}
+		const embed = new MessageEmbed()
+			.setTitle(subName)
+			.setDescription(`"${subreddit.at(x).title}"`)
+			.setColor(client.colors.blue)
+			.setImage(subreddit.at(x).url)
+			.setFooter({ text: `${x}/${subreddit.length}` });
+		const buttons = new MessageActionRow()
+			.addComponents(
+				new MessageButton({ label: '◀', customId: 'previous', style: 'SECONDARY' }),
+				new MessageButton({ label: '▶', customId: 'next', style: 'SECONDARY' }),
+				new MessageButton({ label: '✕', customId: 'close', style: 'DANGER' }),
+			);
+			const imageMessage = await message.reply({ embeds: [embed], components: [buttons] });
+		const filter = i => {
+			i.deferUpdate();
+			return i.user.id === message.author.id;
+		};
+
+		const collector = imageMessage.createMessageComponentCollector({ filter, idle: 60000 });
+		collector.on('collect', i => {
+			switch (i.customId) {
+			case 'close':
+				collector.stop();
+				return;
+			case 'next':
+				if (x < subreddit.length) x++;
+				break;
+			case 'previous':
+				if (x > 0) x--;
+				break;
+			default:
+				return;
+			}
+			imageMessage.edit({ embeds: [
+				embed.setImage(subreddit.at(x).url)
+					.setFooter({ text: `${x + 1}/${subreddit.length}` })
+					.setDescription(`"${subreddit.at(x).title}"`),
+			] });
+		});
+		collector.on('end', (collected, reason) => {
+			if (reason === 'idle') imageMessage.edit({ components: [] });
+			if (reason === 'user') {
+				imageMessage.edit({ embeds: [embed.setImage().setDescription(`"${subName}"\nImage search closed.`)] });
+				imageMessage.edit({ components: [] });
+			}
+		});
+	},
+}
