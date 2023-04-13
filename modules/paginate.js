c98c338fddc0f804c937e8a22b7a9993f557e81c
tatsumara <mara@tatsumara.dev>
2023-04-14 00:22:23 +0200

    feat: allow paginate module to work with both embeds and text

--- modules/paginate.js
+++ modules/paginate.js
@@ -1,13 +1,17 @@
 const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
 
-module.exports = async (interaction, pages) => {
+module.exports = async (interaction, pages = [], contents = []) => {
+	if (pages[0] && contents[0] && pages.length !== contents.length) {
+		throw new Error('pages and contents must have the same amount of entries.');
+	}
+
 	const buttons = new MessageActionRow()
 		.addComponents(
 			new MessageButton({ label: '◀', customId: 'previous', style: 'SECONDARY' }),
 			new MessageButton({ label: '▶', customId: 'next', style: 'SECONDARY' }),
 			new MessageButton({ label: '✕', customId: 'close', style: 'DANGER' }),
 		);
-	const embedMessage = await interaction.editReply({ embeds: [pages[0]], components: [buttons] });
+	const embedMessage = await interaction.editReply({ embeds: pages[0] ? [pages[0]] : [], content: contents[0] ? contents[0] : null, components: [buttons] });
 	const filter = i => {
 		i.deferUpdate();
 		return i.user.id === interaction.user.id;
@@ -21,7 +25,13 @@ module.exports = async (interaction, pages) => {
 			collector.stop();
 			return;
 		case 'next':
-			if (x < pages.length - 1) x++;
+			if (x < pages.length - 1 && x < contents.length - 1) {
+				x++;
+			} else if (x < pages.length - 1) {
+				x++;
+			} else if (x < contents.length - 1) {
+				x++;
+			}
 			break;
 		case 'previous':
 			if (x > 0) x--;
@@ -29,12 +39,12 @@ module.exports = async (interaction, pages) => {
 		default:
 			return;
 		}
-		interaction.editReply({ embeds: [pages[x]] });
+		interaction.editReply({ embeds: pages[x] ? [pages[x]] : [], content: contents[x] ? contents[x] : null });
 	});
 	collector.on('end', (collected, reason) => {
 		if (reason === 'idle') interaction.editReply({ components: [] });
 		if (reason === 'user') {
-			interaction.editReply({ embeds: [new MessageEmbed().setTitle('Closed.')], components: [] });
+			interaction.editReply({ embeds: [new MessageEmbed().setTitle('Closed.')], content: null, components: [] });
 		}
 	});
 };
